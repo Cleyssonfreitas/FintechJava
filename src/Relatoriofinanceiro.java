d517addf114bdab5e4cd1239ea03249815f0f63a
lukeigle <lucasiglesiasmelo@gmail.com>
2025-08-21 19:41:48 -0300

    Create Relatoriofinanceiro.java
    gerar relatórios financeiros

---
+++ src/Relatoriofinanceiro.java
@@ -0,0 +1,10 @@
+public class RelatorioFinanceiro {
+
+    public void gerarExtrato() {
+        System.out.println("Gerando extrato da conta...");
+    }
+
+    public void gerarBalancoAnual() {
+        System.out.println("Gerando balanço anual...");
+    }
+}
